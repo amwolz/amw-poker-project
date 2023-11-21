d8fa9bd0f79be1c62b0d22996f6e67d8d0408599
amwolz <amw21@rice.edu>
2023-11-20 18:29:43 -0700

    m

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 var username = "username";
+var welcomeDict = {"wim1": "images/female1.png",
+                    "wim2": "images/female2.png",
+                    "wim3": "images/male1.png",
+                    "wim4": "images/male2.png"};
+var charSelect = "images/male1.png";
+var wim1 = document.getElementById("wim1");
+var wim2 = document.getElementById("wim2");
+var wim3 = document.getElementById("wim3");
+var wim4 = document.getElementById("wim4");
 
-function storeUsername() {
-    username = document.getElementById("textInput").value;
-    console.log(username)
+function welcomeClick(img) {
+    var div = document.getElementById(img);
+    wim1.style.backgroundColor = "#f4f4f4";
+    wim2.style.backgroundColor = "#f4f4f4";
+    wim3.style.backgroundColor = "#f4f4f4";
+    wim4.style.backgroundColor = "#f4f4f4";    
+    div.style.backgroundColor = "lightcoral";
+    charSelect = welcomeDict[img]
 }
 
+function goToGame() {
+    username = document.getElementById("textInput").value;
+    window.location.href = "index.html"
+}
